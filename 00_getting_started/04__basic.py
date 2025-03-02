a4eeddbc74c0f2d0d75955456a90a0a98069ffed
Syed Bilal Hussain Shah <ahsmirshah@gmail.com>
2025-03-02 12:27:47 +0500

    Refactor docstrings in basic.py for clarity and consistency

--- 00_getting_started/04__basic.py
+++ 00_getting_started/04__basic.py
@@ -12,19 +12,11 @@ async def read():
     return {"message": "This is Syed Bilal"}
 
 
+# Retrieve an item by its ID with an optional query parameter.
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
-    """
-    Retrieve an item by its ID with an optional query parameter.
-
-    Args:
-        item_id (int): The ID of the item (must be an integer).
-        q (Union[str, None], optional): An optional query parameter.
-
-    Returns:
-        dict: A dictionary containing the item ID and query parameter.
-    """
     return {"item_id": item_id, "query": q}
+
 
 # Define a Pydantic model for request validation
 class Item(BaseModel):
@@ -34,7 +26,9 @@ class Item(BaseModel):
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
-    """
+    return {"item_name": item.name, "item_id": item_id}
+
+"""
     Update an item by its ID with provided details.
 
     Args:
@@ -43,5 +37,5 @@ async def update_item(item_id: int, item: Item):
 
     Returns:
         dict: A dictionary containing the updated item details.
-    """
-    return {"item_name": item.name, "item_id": item_id}
+
+"""
